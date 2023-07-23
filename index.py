eda60485f94e98f0e27d5e056df47eff0271c2b6
gusta <gustavo024dias@gmail.com>
2023-07-23 09:56:36 -0300

    first commit

---
+++ index.py
@@ -0,0 +1,46 @@
+from flask import Flask, jsonify, request
+import requests
+
+app = Flask(__name__)
+
+
+@app.route("/requisicao", methods=["POST"])
+def requisitar():
+
+    payload = request.get_json()["payload"]
+
+    fields = '["ValorVenda", "Dormitorios" , "AreaTotal" , "Vagas" , "Status", "Cidade" , "Categoria" ,  "FotoDestaque", "FotoDestaquePequena"]'
+
+    key = payload["key"]
+    host = payload["host"]
+    filter = '{"Codigo": '+str(payload["id_moveis"]).replace("'",'"')+'}'
+
+    response = requests.get(
+        headers={'Accept': 'application/json'},
+        url=host + 'imoveis/listar?key=' + key +
+            '&showtotal=1&pesquisa={"filter":' + filter +
+            ', "fields":' + fields + ',"order":{"Bairro":"asc"},"paginacao":{"pagina":1,"quantidade":50}}'
+    ).json()
+
+    ids = [chave for chave in response.keys() if isinstance(response[chave], dict)]
+
+    objetos_planilhas = []
+
+    for id in ids:
+        objeto = {
+            "Valor": response[id]["ValorVenda"],
+            "Quartos": response[id]["Dormitorios"],
+            "Metros": response[id]["AreaTotal"],
+            "Vagas": response[id]["Vagas"],
+            "Operacao": response[id]["Status"],
+            "Titulo": f"{response[id]['Categoria']} em {response[id]['Cidade']}",
+            "Foto1": response[id]['FotoDestaque'],
+            "Foto2": response[id]['FotoDestaquePequena']
+        }
+        objetos_planilhas.append(objeto)
+
+    return jsonify(objetos_planilhas)
+
+
+if __name__ == "__main__":
+    app.run()
